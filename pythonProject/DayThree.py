4e0afcc2f7a71313044d380cbfd2e8d1a7d1b5f8
NikitaAmerican <157661900+throwAwayingUp@users.noreply.github.com>
2024-12-10 14:49:40 -0500

    Add files via upload

--- pythonProject/DayThree.py
+++ pythonProject/DayThree.py
@@ -1,4 +1,4 @@
-file_path = "C:/Users/BT_4N2_18/PycharmProjects/pythonProject/input.txt"
+file_path = "C:/Users/BT_4N2_18/PycharmProjects/pythonProject/input2.txt"
 with open(file_path, "r") as f:
     content = f.read()
 
@@ -6,29 +6,71 @@ num = ["1","2","3","4","5","6","7","8","9","0"]
 
 
 
-valid = ['m', 'u', 'l', '(', num, ',', num, ')']
+
 
 sum = 0
 index = 0
 for i in range(len(content)):
+    index += 1
     if content[i] == "m":
         if content[i + 1] == "u":
             if content[i + 2] == "l":
                 if content[i + 3] == "(":
                     num1 = ""
                     for j in range(3):
-                        b = j + 5
+                        b = j + 3
                         if content[b] in num:
-                            num1 + content[b]
+                            num1 += content[b]
                     if content[i + 4 + len(num1)] == ",":
                         num2 = ""
                         for j in range(3):
-                            c = j + 5 + len(num1)
+                            c = j + 4 + len(num1)
                             if content[c] in num:
-                                num2 + content[c]
-                        if content[i + 5 + len(num1) + len(num2)] == ")":
-                            sum += int(num1) + int(num2)
+                                num2 += content[c]
+                        if content[i + 5 + len(num1) + len(num2) ] == ")":
+                            print("blah")
+                            sum += int(num1) * int(num2)
 
 print(sum)
-    # maybe mr das was right but I aint backin down
+print(index)
+    # maybe mr das was right but I aint backin down, imma make this work!
 
+# for i in range(len(content)):
+#     if content[i] == "m":
+#         if content[i + 1] == "u":
+#             if content[i + 2] == "l":
+#                 if content[i + 3] == "(":
+#                     num1 = ""
+#                     for j in range(3):
+#                         b = j + 3
+#                         if content[b] in num:
+#                             num1 += content[b]
+#                             # print(content[b])
+#                     print(num1)
+#
+# print(sum)
+
+
+
+# sum = 0
+# index = 0
+# for i in range(len(content)):
+#     if content[i] == "m":
+#         if content[i + 1] == "u":
+#             if content[i + 2] == "l":
+#                 if content[i + 3] == "(":
+#                     num1 = ""
+#                     for j in range(3):
+#                         b = j + 3
+#                         if content[b] in num:
+#                             num1 += content[b]
+#                     if content[i + 4 + len(num1)] == ",":
+#                         num2 = ""
+#                         for j in range(3):
+#                             c = j + 5 + len(num1)
+#                             if content[c] in num:
+#                                 num2 += content[c]
+#                         if content[i + 5 + len(num1) + len(num2)] == ")":
+#                             sum += int(num1) * int(num2)
+#
+# print(sum)
